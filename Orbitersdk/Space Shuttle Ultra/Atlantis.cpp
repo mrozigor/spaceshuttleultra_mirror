mrozigor/spaceshuttleultra_mirror

commit 4efbb7169f66ba74e18ef36bee2cbfcb4667309d

>> moved hydraulic pressure check function to Atlantis.cpp >> made SSME TVC hydraulic dependent
git-svn-id: https://svn.code.sf.net/p/shuttleultra/code/trunk@2577 ee9e6bc9-0c0e-4b95-8483-90109bf3a774

Orbitersdk/Space Shuttle Ultra/Atlantis.cpp
@@ -389,7 +389,7 @@ Atlantis::Atlantis(OBJHANDLE hObj, int fmodel)
 	pgCenter.AddPanel(new vc::PanelC2(this));
 	pgCenter.AddPanel(new vc::PanelC3(this));
 
-	pgRight.AddPanel(panelr2 = new vc::PanelR2(this));
+	pgRight.AddPanel(new vc::PanelR2(this));
 
 	pgOverhead.AddPanel(new vc::PanelO3(this));
 	pgOverhead.AddPanel(new vc::PanelO6(this));
@@ -2563,8 +2563,9 @@ void Atlantis::ToggleGrapple(void)
 	}
 }
 
-bool Atlantis::HydraulicsOK() {
-	return panelr2->HydraulicPressure();
+bool Atlantis::HydraulicsOK( void ) const
+{
+	return ((pAPU[0]->GetHydraulicPressure() >= 2800) || (pAPU[1]->GetHydraulicPressure() >= 2800) || (pAPU[2]->GetHydraulicPressure() >= 2800));
 }
 
 void Atlantis::GimbalOMS(int engine, double pitch, double yaw)
@@ -5088,11 +5089,11 @@ void Atlantis::clbkPostStep(double simt, double simdt, double mjd)
 			break;
 		case STATE_ORBITER: // post tank separation
 			//Check if Control Surfaces are usable
-			if (ControlSurfacesEnabled && !panelr2->HydraulicPressure())
+			if (ControlSurfacesEnabled && !HydraulicsOK())
 			{
 				DisableControlSurfaces();
 			}
-			else if (!ControlSurfacesEnabled && panelr2->HydraulicPressure())
+			else if (!ControlSurfacesEnabled && HydraulicsOK())
 			{
 				EnableControlSurfaces();
 			}
@@ -5121,7 +5122,7 @@ void Atlantis::clbkPostStep(double simt, double simdt, double mjd)
 
 		// ***** Animate speedbrake *****
 
-		if (spdb_status >= AnimState::CLOSING && panelr2->HydraulicPressure()) {
+		if (spdb_status >= AnimState::CLOSING && HydraulicsOK()) {
 			double da = simdt * SPEEDBRAKE_OPERATING_SPEED;
 			if (spdb_status == AnimState::CLOSING) { // retract brake
 				if (spdb_proc > spdb_tgt) spdb_proc = max(spdb_tgt, spdb_proc - da);
@@ -6114,19 +6115,42 @@ bool Atlantis::SetSSMEDir(unsigned short usMPSNo, const VECTOR3& dir)
 
 bool Atlantis::SetSSMEGimbalAngles(unsigned usMPSNo, double degPitch, double degYaw)
 {
-	if (usMPSNo == 0) {
-		return SetSSMEGimbalAngles(1, degPitch, degYaw) &&
-			SetSSMEGimbalAngles(2, degPitch, degYaw) &&
-			SetSSMEGimbalAngles(3, degPitch, degYaw);
-	}
-	else if (usMPSNo > 3) {
-		return false; // error
-	}
-	else {
-		VECTOR3 dir = RotateVectorX(SSMEInstalledNullPos[usMPSNo - 1], range(-10.5, degPitch, 10.5));
-		SSMECurrentPos[usMPSNo - 1] = RotateVectorY(dir, range(-8.5, degYaw, 8.5));
-		UpdateSSMEGimbalAnimations();
-		return SetSSMEDir(usMPSNo, SSMECurrentPos[usMPSNo - 1]);
+	switch (usMPSNo)
+	{
+		case 0:
+			return SetSSMEGimbalAngles(1, degPitch, degYaw) &&
+				SetSSMEGimbalAngles(2, degPitch, degYaw) &&
+				SetSSMEGimbalAngles(3, degPitch, degYaw);
+		case 1:
+			if ((pAPU[0]->GetHydraulicPressure() >= 1500) || (pAPU[2]->GetHydraulicPressure() >= 1500))
+			{
+				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[0], range( -10.5, degPitch, 10.5 ) );
+				SSMECurrentPos[0] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );
+				UpdateSSMEGimbalAnimations();
+				return SetSSMEDir( 1, SSMECurrentPos[0] );
+			}
+			return true;
+		case 2:
+			if ((pAPU[1]->GetHydraulicPressure() >= 1500) || (pAPU[0]->GetHydraulicPressure() >= 1500))
+			{
+				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[1], range( -10.5, degPitch, 10.5 ) );
+				SSMECurrentPos[1] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );
+				UpdateSSMEGimbalAnimations();
+				return SetSSMEDir( 2, SSMECurrentPos[1] );
+			}
+			return true;
+		case 3:
+			if ((pAPU[2]->GetHydraulicPressure() >= 1500) || (pAPU[1]->GetHydraulicPressure() >= 1500))
+			{
+				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[2], range( -10.5, degPitch, 10.5 ) );
+				SSMECurrentPos[2] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );
+				UpdateSSMEGimbalAnimations();
+				return SetSSMEDir( 3, SSMECurrentPos[2] );
+			}
+			return true;
+
+		default:
+			return false; // error
 	}
 }
 

Orbitersdk/Space Shuttle Ultra/Atlantis.h
@@ -560,7 +560,7 @@ public:
 	virtual void StartRSLSSequence();
 	virtual bool GetRSLSAbortFlag() const;
 
-	virtual bool HydraulicsOK();
+	virtual bool HydraulicsOK( void ) const;
 
 	virtual void PSN4( void );
 	virtual void SetSSMEActPos( int num, double Ppos, double Ypos );
@@ -628,8 +628,6 @@ public:
 	ATTACHMENTHANDLE ahET, ahLeftSRB, ahRightSRB;
 	
 	AtlantisSubsystemDirector* psubsystems;
-	
-	vc::PanelR2 *panelr2; // temporary
 
 	/**
 	 * Bridge function between MPS and ET to "deliver" GO2 and GH2 for

Orbitersdk/Space Shuttle Ultra/vc/PanelR2.cpp
@@ -560,9 +560,4 @@ namespace vc
 
 		AtlantisPanel::Realize();
 	}
-
-	bool PanelR2::HydraulicPressure() const
-	{
-		return (APU_HydraulicPress[0].IsSet(2.8f) || APU_HydraulicPress[1].IsSet(2.8f) || APU_HydraulicPress[2].IsSet(2.8f));
-	}
 };

Orbitersdk/Space Shuttle Ultra/vc/PanelR2.h
@@ -80,8 +80,6 @@ namespace vc
 		virtual void RegisterVC();
 		virtual void DefineVC();
 		virtual void Realize();
-
-		virtual bool HydraulicPressure() const;
 	};
 };
 

commit 36624aa378bb54bda251ae7d901f96564ccb4518

>> fixed issue introduced in r2577 that caused SSME position not to be set at scenario start >> corrected SSME position in several scenarios
git-svn-id: https://svn.code.sf.net/p/shuttleultra/code/trunk@2595 ee9e6bc9-0c0e-4b95-8483-90109bf3a774

Orbitersdk/Space Shuttle Ultra/Atlantis.cpp
@@ -6146,6 +6146,7 @@ bool Atlantis::SetSSMEDir(unsigned short usMPSNo, const VECTOR3& dir)
 
 bool Atlantis::SetSSMEGimbalAngles(unsigned usMPSNo, double degPitch, double degYaw)
 {
+	// HACK has temporary exception for the start of the simulation, so scenario setting can be used
 	switch (usMPSNo)
 	{
 		case 0:
@@ -6153,7 +6154,7 @@ bool Atlantis::SetSSMEGimbalAngles(unsigned usMPSNo, double degPitch, double deg
 				SetSSMEGimbalAngles(2, degPitch, degYaw) &&
 				SetSSMEGimbalAngles(3, degPitch, degYaw);
 		case 1:
-			if ((pAPU[0]->GetHydraulicPressure() >= 1500) || (pAPU[2]->GetHydraulicPressure() >= 1500))
+			if ((pAPU[0]->GetHydraulicPressure() >= 1500) || (pAPU[2]->GetHydraulicPressure() >= 1500) || (oapiGetSimTime() == 0.0))
 			{
 				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[0], range( -10.5, degPitch, 10.5 ) );
 				SSMECurrentPos[0] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );
@@ -6162,7 +6163,7 @@ bool Atlantis::SetSSMEGimbalAngles(unsigned usMPSNo, double degPitch, double deg
 			}
 			return true;
 		case 2:
-			if ((pAPU[1]->GetHydraulicPressure() >= 1500) || (pAPU[0]->GetHydraulicPressure() >= 1500))
+			if ((pAPU[1]->GetHydraulicPressure() >= 1500) || (pAPU[0]->GetHydraulicPressure() >= 1500) || (oapiGetSimTime() == 0.0))
 			{
 				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[1], range( -10.5, degPitch, 10.5 ) );
 				SSMECurrentPos[1] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );
@@ -6171,7 +6172,7 @@ bool Atlantis::SetSSMEGimbalAngles(unsigned usMPSNo, double degPitch, double deg
 			}
 			return true;
 		case 3:
-			if ((pAPU[2]->GetHydraulicPressure() >= 1500) || (pAPU[1]->GetHydraulicPressure() >= 1500))
+			if ((pAPU[2]->GetHydraulicPressure() >= 1500) || (pAPU[1]->GetHydraulicPressure() >= 1500) || (oapiGetSimTime() == 0.0))
 			{
 				VECTOR3 dir = RotateVectorX( SSMEInstalledNullPos[2], range( -10.5, degPitch, 10.5 ) );
 				SSMECurrentPos[2] = RotateVectorY( dir, range( -8.5, degYaw, 8.5 ) );

Scenarios/Space Shuttle Ultra/Historic missions/STS-107/Post-insertion PLBD opening.scn
@@ -280,6 +280,9 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM ADPS
   LEFT_AIRDATAPROBE 1 3 0.000000

Scenarios/Space Shuttle Ultra/Historic missions/STS-114/Post-OMS2.scn
@@ -289,6 +289,9 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM ADPS
   LEFT_AIRDATAPROBE 1 3 0.000000

Scenarios/Space Shuttle Ultra/Historic missions/STS-126/STS-126 docking with ISS Nov 16.scn
@@ -303,6 +303,9 @@ Endeavour:SpaceShuttleUltra
   CONTROL_MODE 0
   CUR_MNVR 1 1.570796 0.000000 -0.000000
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM RMS
   ARM_STATUS 0.499999 0.183685 0.167706 0.520596 0.500000 0.500000

Scenarios/Space Shuttle Ultra/Testing scenarios/ISS Radar Test.scn
@@ -250,7 +250,7 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @BEGINSOFTWARE IO_Control
   @ENDSOFTWARE 
   @BEGINSOFTWARE ATVC_SOP
-  CONFIG 1
+  CONFIG 2
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 

Scenarios/Space Shuttle Ultra/Theoretical missions/STS-61F/Centaur rotation to deploy.scn
@@ -236,6 +236,9 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM ADPS
   LEFT_AIRDATAPROBE 1 3 0.000000

Scenarios/Space Shuttle Ultra/Theoretical missions/STS-61F/Challenger in orbit.scn
@@ -236,6 +236,9 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM ADPS
   LEFT_AIRDATAPROBE 1 3 0.000000

Scenarios/Space Shuttle Ultra/Theoretical missions/STS-81I/Atlantis in orbit.scn
@@ -243,6 +243,9 @@ EVENT_TIMER1 0.000000 DOWN STOPPED
   @ENDSOFTWARE 
   @BEGINSOFTWARE GeneralDisplays
   @ENDSOFTWARE 
+  @BEGINSOFTWARE ATVC_SOP
+  CONFIG 2
+  @ENDSOFTWARE 
 @ENDSUBSYSTEM		;SimpleGPCSystem
 @SUBSYSTEM ADPS
   LEFT_AIRDATAPROBE 1 3 0.000000